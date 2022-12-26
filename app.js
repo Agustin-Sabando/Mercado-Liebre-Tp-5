b9e3392fb43880b70220cb1f015d613e363936f2
Agustin-Sabando <agustinsabando18@gmail.com>
2022-12-26 17:19:07 -0300

    Subiendo tp Nro 6 y 7

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3030;
+const port = 5000;
 
 app.use(express.static(path.resolve(__dirname,'public')));
 
-app.get('/',(req, res) => res.sendFile(path.resolve(__dirname,'views', 'index.html')));
+app.get('/',(req, res) => res.sendFile(path.resolve(__dirname,'views', 'home.html')));
 
 
 app.listen(port, () => console.log(`Se levanto con exito el servidor en http://localhost:${port}`));
